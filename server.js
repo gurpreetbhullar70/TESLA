a22e301b579bdd0e983a0e3c365c2e35d18202d8
preetbhullar90 <62619558+preetbhullar90@users.noreply.github.com>
2022-11-03 16:06:33 +0000

    tesla data

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json())
 
 
+
 app.use((req, res, next)=>{
     res.setHeader('Acce ss-Control-Allow-Origin', '*'),
     res.setHeader('Access-Control-Allow-Headers', '*'),
